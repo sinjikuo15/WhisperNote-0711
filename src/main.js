772c4c5a12bed4efe3c4fbcd53f10d85e8fb1da8
Kelly Chang <u2kelly619@gmail.com>
2022-07-08 11:51:06 +0800

    1. Fix ckeditor tailwind bug 2. Modify addNewDiary style 3. Send addNewDiary date to backend

--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-import CKEditor from '@ckeditor/ckeditor5-vue'
+import CKEditor from '@ckeditor/ckeditor5-vue';
 import { vfmPlugin } from 'vue-final-modal'
 import './assets/tailwind.css'
+import './assets/style.css'
 
 createApp(App).use(router).use(VueAxios, axios).use(CKEditor).use(vfmPlugin).mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -49,35 +49,69 @@
     </div>
   </div>
 
-  
-
+<!-- modal -->
   <button class="add-note-btn" @click="showModal = true">
     <PlusIcon class="h-8 w-5"/>
   </button>
   <vue-final-modal v-model="showModal" classes="modal-container" content-class="modal-content">
     <button class="modal__close" @click="showModal = false">
-      X
+      <XIcon class="h-8 w-5"/>
     </button>
-    <span class="modal__title">新增日記</span>
+    <span class="modal__title text-center text-xl leading-6 font-bold text-gray-900">新增日記</span>
     <div class="modal__content">
-      <p>2022/7/4</p>
-      <p>標題</p>
-      <input type="text" placeholder="請輸入標題" v-model="dairyTitle">
-      <p>標籤</p>
-      <select name="groupTag" id="groupTag" v-model="groupTag">
-        <option value="">請選擇</option>
-        <option value="工作">工作</option>
-        <option value="大學">大學</option>
-      </select>
-      <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
-      <!-- <button @click="showHtml">show content HTML</button> -->
+      <label class="block mt-3">
+        <span class="text-gray-700">標題</span>
+        <input type="text" class="
+            mt-1
+            block
+            w-full
+            rounded-md
+            border-gray-300
+            shadow-sm
+            focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
+          " placeholder="" v-model="diaryTitle">
+      </label>
+      <label class="block mt-3">
+        <span class="text-gray-700">日期</span>
+        <input type="date" class="
+            mt-1
+            block
+            w-full
+            rounded-md
+            border-gray-300
+            shadow-sm
+            focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
+          " v-model="date">
+      </label>
+
+      <label class="block mt-3">
+        <span class="text-gray-700">標籤</span>
+        <select class="
+            block
+            w-full
+            mt-1
+            rounded-md
+            border-gray-300
+            shadow-sm
+            focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
+          " name="groupTag" id="groupTag" v-model="groupTag">
+          <option value="">請選擇</option>
+          <option value="工作">工作</option>
+          <option value="大學">大學</option>
+        </select>
+      </label>
+      <div class="mt-5">
+        <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
+      </div>
     </div>
-    <div class="modal__action">
-      <button @click="addNewDairy">confirm</button>
-      <button @click="showModal = false">cancel</button>
+    <div class="flex justify-end">
+      <div class="modal__action">
+        <button type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm" @click.prevent="addNewDiary">confirm</button>
+        <button type="button"  class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm" @click="showModal = false">cancel</button>
+      </div>
     </div>
+    
   </vue-final-modal>
-      
 
 </template>
 
@@ -85,13 +119,15 @@
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { $vfm, VueFinalModal, ModalsContainer } from 'vue-final-modal'
 import { PlusIcon } from '@heroicons/vue/solid'
+import { XIcon } from '@heroicons/vue/solid'
 
 export default {
     name: 'app',
     components: {
       VueFinalModal,
       ModalsContainer,
-      PlusIcon 
+      PlusIcon,
+      XIcon,
     },
     data() {
         return {
@@ -101,18 +137,22 @@ export default {
                 // The configuration of the editor.
             },
             showModal: false,
-            dairyTitle: '',
-            groupTag: '',
+            diaryTitle: '',
+            groupTag: '', //要再抓出該使用者的permission，迴圈顯示option
+            date: '',
+            inputDate: ''
         };
     },
     methods: {
-      addNewDairy(){
-        const dairyDetail = {
-          title: this.dairyTitle,
+      addNewDiary(){
+        const diaryDetail = {
+          title: this.diaryTitle,
           permission: this.groupTag,
-          content: this.editorData
+          content: this.editorData,
+          date: this.date
         }
-        this.axios.post('/add-dairy', dairyDetail)
+        console.log(diaryDetail)
+        this.axios.post('/add-diary', diaryDetail)
           .then((res) => {
             console.log(res.data)
           })
@@ -120,6 +160,15 @@ export default {
             console.log(err);
           })
       }
+    },
+    mounted(){
+      let now = new Date();
+      let year = now.getFullYear();
+      let month = now.getMonth()+1;
+      if (month<10) month="0"+month
+      let date = now.getDate();
+      if (date<10) date="0"+date
+      this.date = year+"-"+month+"-"+date
     }
 }
 
@@ -189,23 +238,23 @@ border-radius: 10px 0px 0px 10px;
   border-radius: 0.25rem;
   background: #fff;
 }
-.modal__title {
+/* .modal__title {
   margin: 0 1.5rem 1rem 0;
   font-size: 1.5rem;
   font-weight: 700;
-}
+} */
 .modal__content {
   flex-grow: 1;
   overflow-y: auto;
   text-align: left;
 }
-.modal__action {
+/* .modal__action {
   display: flex;
   justify-content: center;
   align-items: center;
   flex-shrink: 0;
   padding: 1rem 0 0;
-}
+} */
 .modal__close {
   position: absolute;
   top: 1rem;

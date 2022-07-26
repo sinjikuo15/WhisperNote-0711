7109814315f85823e736ca3a819e93394c35c7a3
shin.kuo15 <shin.kuo15@gmail.com>
2022-07-26 15:10:52 +0800

    Signup 失敗時顯示的內容

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -41,16 +41,41 @@
                             <div class="flex sm:block md:flex justify-center  py-4">
                                 <div class="flex justify-center ">
                                     <p class="text-gray-700 text-xs text-start"> 點擊註冊代表您同意<b class="text-blue-600">WhisperNote</b>之會員與客戶隱私權條款</p>
-                                </div>                             
-                                
+                                </div>                          
+                               
                             </div>
+                            <!-- 註冊失敗顯示內容 -->
+                            <div  v-if="userExistMsg" id="alert-1" class="flex p-4 mb-4 bg-red-100 rounded-lg " role="alert">
+                                <svg aria-hidden="true" class="flex-shrink-0 w-5 h-5 text-red-700" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+                                <span class="sr-only">Info</span>
+                                <div class="ml-3 text-sm font-medium text-red-700">
+                                    <span class="text-red-700">{{userExistMsg}}</span> 
+                                </div>
+                                <!-- <button type="button" class="ml-auto -mx-1.5 -my-1.5 bg-red-100 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex h-8 w-8 " data-dismiss-target="#alert-1" aria-label="Close">
+                                    <span class="sr-only">Close</span>
+                                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                </button> -->
+                            </div>
+                            <!-- 登入成功顯示內容 -->
+                            <!-- <div v-if="userCreateSuccessMsg" id="alert-2" class="flex p-4 mb-4 bg-green-100 rounded-lg" role="alert">
+                                <svg aria-hidden="true" class="flex-shrink-0 w-5 h-5 text-green-700 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+                                <span class="sr-only">Info</span>
+                                <div class="ml-3 text-sm font-medium text-green-700">
+                                   <span class="text-green-700">{{userCreateSuccessMsg}}</span>
+                                </div>
+                                <button type="button" class="ml-auto -mx-1.5 -my-1.5 bg-green-100 text-green-500 rounded-lg focus:ring-2 focus:ring-green-400 p-1.5 hover:bg-green-200 inline-flex h-8 w-8 " data-dismiss-target="#alert-2" aria-label="Close">
+                                    <span class="sr-only">Close</span>
+                                    <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                </button>
+                            </div>                              -->
                              <!-- 樣式要再改為tailwind寫法 -->
-                            <div v-if="userExistMsg" class="alert alert-danger" role="alert">
+
+                            <!-- <div v-if="userExistMsg" class="alert alert-danger" role="alert">
                                 <span>{{userExistMsg}}</span>
-                            </div>
-                            <div v-if="userCreateSuccessMsg" class="alert alert-success" role="alert">
+                            </div> -->
+                            <!-- <div v-if="userCreateSuccessMsg" class="alert alert-success" role="alert">
                                 <span>{{userCreateSuccessMsg}}</span>
-                            </div>  
+                            </div>   -->
                             <button class="w-full btn-primary font-bold p-2 mt-1 mb-1" @click.prevent="postSignup">註冊</button>
                         </div>                        
                     </div>                
